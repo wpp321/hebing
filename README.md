dce4cd12c9492e4e109d2c04973b82d848336be4
jay <wxxxj2018@163.com>
2022-11-13 20:50:56 +0800

    添加readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hebing
+将多个python文件合并为一个文件
+## 安装
+```
+pip install hebing
+```
+## 使用
+```
+hebing a.py b.by
+```
+`a.py` 是要合并的文件，`b.py`为合并生成的文件。执行此命令后，`a.py`文件及其依赖的其他本地`python`文件会合并生成一个新文件`b.py`。
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hebing",
-    version="0.0.1",
+    version="0.0.2",
     author="WangXueJie",
     author_email="hajiew@163.com",
     description="merge multiple python files into one python file",
@@ -11,5 +11,5 @@ setup(
     install_requires=[
         "chardet~=5.0.0"
     ],
-    entry_points={'console_scripts': ['allinone=allinone:execute']}
+    entry_points={'console_scripts': ['hebing=hebing:execute']}
 )
